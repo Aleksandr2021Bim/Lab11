a106c9febfaf4538b3bf8edf6bc2b1d6ec1e2582
Aleksandr Panarin <panarinalex12@gmail.com>
2021-12-15 13:03:26 +0300

    Добавьте файлы проекта.

---
+++ Задание 11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет инженер \nметод ввиде структур");
+            LinEgua linEgua = new LinEgua();
+            Console.WriteLine("Введите число k");
+            Console.WriteLine("Введите число b");
+            linEgua.Root();
+            Console.WriteLine("Значение х =", x);
+            Console.ReadKey();
+        }
+
+        struct LinEgua //создание структуры
+        {
+            public double k = Convert.ToDouble(Console.ReadLine());
+            public double b = Convert.ToDouble(Console.ReadLine());
+            public double x = 0;
+
+            public void Root() // метод в структуре
+            { if ((k != 0) && (b != 0))
+                {
+                    Console.Write("Решать по формуле");
+                    x += - b / k;
+                    break;
+                    if ((k = 0) && (b = 0))
+                    {
+                        Console.Write("х - любое число");
+                        break;
+                        if ((k = 0) && (b != 0))
+                        {
+                            Console.Write("х - нет решения");
+                            break;
+                        }
+                    }
+                    return;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ задание 11.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_11._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Привет инженер \nметод ввиде структур");
+            Console.WriteLine("Введите число k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            LinEgua linEgua = new LinEgua(k, b);
+            Console.WriteLine(linEgua.Root());
+            Console.ReadKey();
+        }
+    }
+
+        struct LinEgua //создание структуры
+        {
+            public double k ;
+            public double b ;
+            
+            public LinEgua(double k, double b)
+            {
+                this.k = k;
+                this.b = b;
+            }
+
+
+            public string Root() // метод в структуре
+            {
+                string result = "";
+                if ((k != 0) && (b != 0))
+                      result = (-b / k).ToString();
+                else if ((k == 0) && (b == 0))
+                      result = "х - любое число";
+                else if ((k == 0) && (b != 0))
+                        result = "х - нет решения";
+                      return result;
+            }
+                
+        }
+}
+    
+
